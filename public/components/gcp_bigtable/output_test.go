78be76f14bf6467be030e72707b2afc6e31ac6ae
Daan Gerits <daan.gerits@gmail.com>
2024-12-10 15:04:52 +0100

    added tests for gcp_bigtable
    Signed-off-by: Daan Gerits <daan.gerits@gmail.com>

--- public/components/gcp_bigtable/output_test.go
+++ public/components/gcp_bigtable/output_test.go
@@ -19,7 +19,13 @@ var _ = Describe("Output", func() {
 	BeforeEach(func() {
 		// create a test table
 		tableName = fmt.Sprintf("test-table-%s", uuid.NewString())
-		err := btac.CreateTable(context.Background(), "test-table")
+		err := btac.CreateTable(context.Background(), tableName)
+		Expect(err).NotTo(HaveOccurred())
+
+		err = btac.CreateColumnFamily(context.Background(), tableName, "general")
+		Expect(err).NotTo(HaveOccurred())
+
+		err = btac.CreateColumnFamily(context.Background(), tableName, "invoices")
 		Expect(err).NotTo(HaveOccurred())
 	})
 
@@ -35,7 +41,7 @@ input:
   generate:
     count: 1
     mapping: |
-      root.key = uuid_v4()
+      root.key = "my/test/key"
       root.general.name = "Daan Gerits"
       root.general.age = 25
       root.invoices.INV001.amount = 100
@@ -45,15 +51,16 @@ input:
 
 output:
   gcp_bigtable:
+    emulated_host_port: %s
     project: fake-project
     instance: fake-instance
     table: %s
-    row_key: this.key
-    credentials_json: whatever
-`, tableName))
+    key: this.key
+    data: this.without("key")
+`, srv.Addr, tableName))
 	})
 
-	It("should create a new output", func() {
+	It("should write the data to bigtable", func() {
 		sb := service.NewStreamBuilder()
 		Expect(sb.SetYAML(config)).To(Succeed())
 
@@ -62,5 +69,33 @@ output:
 
 		err = stream.Run(context.Background())
 		Expect(err).NotTo(HaveOccurred())
+
+		tbl := btc.Open(tableName)
+		row, err := tbl.ReadRow(context.Background(), "my/test/key")
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(row.Key()).To(Equal("my/test/key"))
+
+		general := row["general"]
+		Expect(general).To(HaveLen(2))
+
+		ri := general[0]
+		Expect(ri.Column).To(Equal("general:age"))
+		Expect(ri.Value).To(Equal([]byte("25")))
+
+		ri = general[1]
+		Expect(ri.Column).To(Equal("general:name"))
+		Expect(ri.Value).To(Equal([]byte("\"Daan Gerits\"")))
+
+		invoices := row["invoices"]
+		Expect(invoices).To(HaveLen(2))
+
+		ri = invoices[0]
+		Expect(ri.Column).To(Equal("invoices:INV001"))
+		Expect(ri.Value).To(Equal([]byte(`{"amount":100,"date":"2021-01-01"}`)))
+
+		ri = invoices[1]
+		Expect(ri.Column).To(Equal("invoices:INV002"))
+		Expect(ri.Value).To(Equal([]byte(`{"amount":200,"date":"2021-01-02"}`)))
 	})
 })
